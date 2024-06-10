c03535ade9f0ec89a7f6933b1fd4b82ada86c3b7
Jareeex <baswarafajar@gmail.com>
2024-06-11 06:03:57 +0700

    task04.js

--- task04.js
+++ task04.js
@@ -1,8 +1,14 @@
+// Mengambil data pengguna dari URL yang disediakan
 fetch('https://jsonplaceholder.typicode.com/users')
+  // Mengubah respons menjadi JSON
   .then(response => response.json())
+  // Menangani data yang diterima
   .then(data => {
+    // Melakukan iterasi melalui setiap pengguna
     data.forEach(user => {
+      // Menampilkan nama pengguna ke konsol
       console.log(user.name);
     });
   })
-  .catch(error => console.error('Fetch error:', error))
+  // Menangani kesalahan jika terjadi
+  .catch(error => console.error('Fetch error:', error));
