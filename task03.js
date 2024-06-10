4530d0bc152a98af6bbd53b8170a26877ea18c9f
Jareeex <baswarafajar@gmail.com>
2024-06-11 06:01:58 +0700

    task03.js

--- task03.js
+++ task03.js
@@ -1,40 +1,46 @@
+/ Fungsi untuk mendapatkan daftar orang yang piket pada hari Senin
 const piketSenen = () => { 
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // Daftar orang yang piket pada hari Senin
             const piket = ['adit', 'agus'];
             let result = {
                 message: 'ini adalah nama yang piket hari Senin',
                 piket: piket
             };
-            resolve(result);
-        }, 2000);
+            resolve(result); // Mengembalikan hasil ke promise
+        }, 2000); // Waktu simulasi
     });
 };
-// program 1
 
+// Fungsi untuk mendapatkan daftar orang yang piket pada hari Selasa
 const piketSelasa = () => { 
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            // Daftar orang yang piket pada hari Selasa
             const piket2 = ['sopo', 'jarwo'];
             let result = {
                 message: 'ini adalah nama yang piket hari Selasa',
                 piket: piket2
             };
-            resolve(result);
-        }, 1000);
+            resolve(result); // Mengembalikan hasil ke promise
+        }, 1000); // Waktu simulasi
     });
 };
-// program 2
 
+// Menjalankan kedua fungsi secara paralel menggunakan Promise.all()
 Promise.all([piketSenen(), piketSelasa()])
     .then(results => {
         let resultSatu = results[0];
         let resultDua = results[1];
+        // Menampilkan pesan dan daftar orang yang piket pada hari Senin
         console.log(resultSatu.message);
         console.log("Nama yang piket hari Senin:", resultSatu.piket);
+        // Menampilkan pesan dan daftar orang yang piket pada hari Selasa
         console.log(resultDua.message);
         console.log("Nama yang piket hari Selasa:", resultDua.piket);
     })
     .catch(error => {
+        // Menangani kesalahan jika terjadi
         console.error("Terjadi kesalahan:", error);
     });
